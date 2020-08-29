8d3ddc48f3c5646fc466a9c9b6ed71e8515915cc
Ian Hart <ianhart7790@gmail.com>
2020-08-29 16:21:44 -0400

    fixed nav

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,11 +1,7 @@
-import M from 'materialize-css';
-
-document.addEventListener('DOMContentLoaded', function () {
-  console.log(M);
-
-  var elems = document.querySelectorAll('.sidenav');
-  var instances = M.Sidenav.init(elems, {});
-});
+// document.addEventListener('DOMContentLoaded', function () {
+//   var elems = document.querySelectorAll('.sidenav');
+//   var instances = M.Sidenav.init(elems, {});
+// });
 
 if (location.href.includes('?code')) {
   for (let i = 0; i <= 1; i++) {
